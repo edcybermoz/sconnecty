72e9ad75bdf7f4bce4dad4a72e05f2d8cc859237
edcybermoz <edsonantonionhanombe39@gmail.com>
2026-02-19 13:01:42 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Admin from "./pages/Admin";
-import Historico from './pages/Historico';
+import Historico from "./pages/Historico";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -19,7 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/admin" element={<Admin />} />
-          <Route path="/historico" element={<Historico />} />
+          <Route path="/Historico" element={<Historico />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
